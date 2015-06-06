e21ff8d6be200a27650a565166fe8408fd0665d6
David Wu <vladylo98@gmail.com>
2015-06-06 10:10:22 +0800

    commit for Test 1

--- app/src/main/java/idv/david/githubtest/MainActivity.java
+++ app/src/main/java/idv/david/githubtest/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    // Test 1
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
